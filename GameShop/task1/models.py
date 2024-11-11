d72dc140a25707f91691d9c717b4f9e1da846549
Aleksandr <alex.kharchenko@mail.ru>
2024-11-11 12:52:08 +0300

    Add files via upload

--- GameShop/task1/models.py
+++ GameShop/task1/models.py
@@ -13,11 +13,12 @@ class Buyer(models.Model):
 
 
 class Game(models.Model):
-    title = models.CharField(max_length=100)
-    cost = models.DecimalField(max_digits=7, decimal_places=2)
-    size = models.DecimalField(max_digits=7, decimal_places=2)
-    description = models.TextField()
-    age_limited = models.BooleanField(default=False)
+    title = models.CharField(max_length=100)  # Название игры
+    cost = models.DecimalField(max_digits=7, decimal_places=2)  # Стоимость
+    size = models.DecimalField(max_digits=7, decimal_places=2)  # Размер
+    description = models.TextField()  # Описание
+    age_limited = models.BooleanField(default=False)  # Возрастное ограничение
+    buyer = models.ManyToManyField(Buyer, related_name='game')
 
     def __str__(self):
         return self.title
